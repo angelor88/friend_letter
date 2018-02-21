29fc689afe94686934dbb6fc60384bd059b0b00e
angelor88 <angelorussoniello88@gmail.com>
2018-02-20 17:14:18 -0800

    A letter for a friend

---
+++ Startup.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FriendLetter
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+// the code in Configure() tells our app how to use each of the frameworks we use to handle requests to the server.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+/*inside of Configure() to tell our app that it will be using the MVC framework to respond to HTTP requests.*/
+// ASP.NET calls Configure when the app starts
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
